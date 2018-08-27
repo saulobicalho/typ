3ec3321c5f81e5c414c440778adcffe2822055d5
Saulo Weikert BIcalho <sbicalho86@gmail.com>
2018-08-27 15:36:56 -0300

    2.9 f

--- ts/models/Negociacao.ts
+++ ts/models/Negociacao.ts
@@ -3,16 +3,6 @@ class Negociacao{
 
   constructor(private _data: Date, private _quantidade: number, private _valor: number){
 
-    if(!_data){
-      throw new Error('data deve ser preenchida');
-    }
-    if(!_quantidade){
-      throw new Error('quantidade deve ser preenchida');
-    }
-    if(!_valor){
-      throw new Error('valor deve ser preenchida');
-    }
-
     this._data = _data;
     this._quantidade = _quantidade;
     this._valor = _valor;
