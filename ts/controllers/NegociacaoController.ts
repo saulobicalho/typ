e0dc4e3ed1e25485c4a8e5230a0960ff64aae995
Saulo Weikert BIcalho <sbicalho86@gmail.com>
2018-08-27 15:26:14 -0300

    2.7 i

--- ts/controllers/NegociacaoController.ts
+++ ts/controllers/NegociacaoController.ts
@@ -1,8 +1,8 @@
 class NegociacaoController{
 
-  private _inputData;
-  private _inputQuantidade;
-  private _inputValor;
+  private _inputData: Element;
+  private _inputQuantidade: Element;
+  private _inputValor: Element;
 
   constructor(){
     this._inputData = document.querySelector('#data');
@@ -10,7 +10,7 @@ class NegociacaoController{
     this._inputValor = document.querySelector('#valor');
 
   }
-  adiciona(event){
+  adiciona(event: Event){
     event.preventDefault();
     const negociacao = new Negociacao(
       this._inputData.value,
