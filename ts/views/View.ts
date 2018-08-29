b44e4a24254353b74c10f8868f1e6e954e3da916
Saulo Weikert BIcalho <sbicalho86@gmail.com>
2018-08-29 09:13:09 -0300

    4.4

--- ts/views/View.ts
+++ ts/views/View.ts
@@ -6,4 +6,14 @@ class View{
 
     this._elemento = document.querySelector(seletor);
   }
+
+  update(model: string){
+
+    this._elemento.innerHTML = this.template(model);
+  }
+
+  template(model:string){
+
+    throw new Error('Voce deve implementar o metodo template');
+  }
 }
