d77cb78fbc2c33ecaac97caa5794b0dbb0bbb9ee
Saulo Weikert BIcalho <sbicalho86@gmail.com>
2018-08-29 15:49:28 -0300

    consertando app.ts

--- ts/controllers/NegociacaoController.ts
+++ ts/controllers/NegociacaoController.ts
@@ -17,9 +17,9 @@ class NegociacaoController{
   adiciona(event: Event){
     event.preventDefault();
     const negociacao = new Negociacao(
-      new Date(this._inputData.value.replace(/-/g,',')),
-      parseInt(this._inputQuantidade.value),
-      parseInt(this._inputValor.value)
+      new Date(this._inputData.val().replace(/-/g,',')),
+      parseInt(this._inputQuantidade.val()),
+      parseInt(this._inputValor.val())
     );
 
     this._negociacoes.adiciona(negociacao);
